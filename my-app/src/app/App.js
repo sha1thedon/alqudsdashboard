cb9bc045ec79c084c435d61c7b8e0014b1b74559
Shawon Sheikh <shawonsheikh@Shawons-MacBook-Pro.local>
2023-10-14 14:21:44 +0100

    added react components

---
+++ my-app/src/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from './Components/Loginpage';
+
+const App = () => {
+    return (
+      <div>
+        <h1>Hello from LoginPage!</h1>
+        < Login/>
+      </div>
+    );
+  };
+
+export default App;
---
+++ my-app/src/app/Components/Loginpage.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log(`Username: ${username}`);
+    console.log(`Password: ${password}`);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
